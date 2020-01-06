2e492dc4c71017bf9aea5a1a3a3a858387e54117
david max <davidmax.dmao@gmail.com>
2020-01-06 17:00:44 -0300

    Foi adicionada a pagina de detalhes e um campo de pesquisa

--- animais/urls.py
+++ animais/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('cadastrar_animal/', CadastrarAnimaisView.as_view(), name='cadastrar_animal'),
     path('editar_animal/<int:pk>', EditarAnimaisView.as_view(), name='editar_animal'),
     path('remover_animal/<int:pk>', RemoverAnimalView.as_view(), name='remover_animal'),
+    path('detalhe_animal/<int:pk>', DetalheAnimalView.as_view(), name='detalhe_animal'),
+    path('busca_animal/', BuscaAnimalView.as_view(), name='busca_animal'),
 
 ]
+
--- animais/views.py
+++ animais/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView, DetailView)
 
 from .models import Animal
 from .forms import AnimalForm
@@ -29,5 +30,28 @@ class EditarAnimaisView(UpdateView):
 
 class RemoverAnimalView(DeleteView):
     model = Animal
-    template_name = 'confirmar_exclusao_adocao.html'
+    template_name = 'confirmar_exclusao.html'
     success_url = reverse_lazy('listar_animais')
+
+
+class DetalheAnimalView(DetailView):
+    model = Animal
+    template_name = 'detalhe_animal.html'
+
+
+
+
+class BuscaAnimalView(ListView):
+    model = Animal
+    template_name = 'busca_animal.html'
+
+
+    def get_queryset(self):
+        query = self.request.GET.get('nome')
+
+        return Animal.objects.get(nome=query)
+
+
+
+
+
